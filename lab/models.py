4bc605d80e48b1597e6b87cac94883807854f65f
Geovani Mendonca <geovani.torezin11@gmail.com>
2020-07-03 18:20:58 -0300

    .

--- lab/models.py
+++ lab/models.py
@@ -26,7 +26,6 @@ class Post(models.Model):
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    paciente = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
