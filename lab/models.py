c0a1ed14e2d19a301f5c3ad5e9f2c246a1d075f8
Geovani Mendonca <geovani.torezin11@gmail.com>
2020-07-03 17:42:24 -0300

    final

--- lab/models.py
+++ lab/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 
 
 
+'''
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     Exame = models.CharField(max_length=150)
@@ -20,11 +21,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.Exame
-    
-'''
+'''   
+
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Exame = models.CharField(max_length=150)
+    Paciente = models.CharField(max_length=150)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
@@ -34,6 +36,5 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.user
     
-'''
