78983c3304385386132837e4aaf9cd0040dfc8e0
Geovani Mendonca <geovani.torezin11@gmail.com>
2020-07-03 18:17:13 -0300

    .

--- lab/forms.py
+++ lab/forms.py
@@ -6,4 +6,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('author', 'exame','paciente','text',)
+        fields = ('author', 'title','paciente','text',)
