58d0c03c7022699f3c4c1396966c1c073a748499
Geovani Mendonca <geovani.torezin11@gmail.com>
2020-07-03 17:23:29 -0300

    Login, Logout, Register e posts

--- lab/models.py
+++ lab/models.py
@@ -1,8 +1,27 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Exame = models.CharField(max_length=150)
+    Paciente = models.CharField(max_length=150)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.Exame
+    
+'''
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -17,3 +36,4 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+'''
